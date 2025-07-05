cd2fc241025c9924cc89c7985caf4c0826b8ff81
starstalker9160 <abyazjschool@gmail.com>
2025-07-05 12:29:24 +0400

    moved stuff + calculator app

---
+++ calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by star.stalker9160 on 05/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ calculator/calculatorTests/calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorTests.swift
+//  calculatorTests
+//
+//  Created by star.stalker9160 on 05/07/2025.
+//
+
+import Testing
+@testable import calculator
+
+struct calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
