639be9844fce7f5c6c489e19ef235ba24d6bc880
starstalker9160 <abyazjschool@gmail.com>
2025-07-05 12:55:56 +0400

    Add calculator - Playground

---
+++ calculator - Playground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let x: Int = 34
+var y: Int = 9
+
+print("sum: \(x+y)")
+print("difference: \(x-y)")
+print("product: \(x*y)")
+print("quotient: \(x/y)")
+print("remainder: \(x%y)")
+
+print("\n\n")
+
+print("incrementing y by 5")
+print("current y value: \(y)")
+y += 5
+print("new y value: \(y)")
