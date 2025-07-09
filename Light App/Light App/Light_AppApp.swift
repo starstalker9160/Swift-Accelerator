1687addf10e38b410dde0ec8888b3d870d2704ee
starstalker9160 <abyazjschool@gmail.com>
2025-07-09 13:26:19 +0900

    light app

---
+++ Light App/Light App/Light_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Light_AppApp.swift
+//  Light App
+//
+//  Created by star.stalker9160 on 09/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Light_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
