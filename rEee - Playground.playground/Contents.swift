286a86d0da6b38f91e2541e335a6b7e94727de4f
starstalker9160 <abyazjschool@gmail.com>
2025-07-09 10:49:36 +0900

    5

--- rEee - Playground.playground/Contents.swift
+++ rEee - Playground.playground/Contents.swift
@@ -1,5 +1,31 @@
 import UIKit
 
-var name = "Abyaz"
+// If - else
+var age: Int = 19
+if (age > 18) {
+    print("You are eligible to vote\n")
+} else {
+    print("You are NOT eligible to vote\n")
+}
 
-print("Hello from \(name)! \r\r\n;lkj")
+
+// If - else if - else
+let number: Int = 0
+if (number > 0) {
+    print("Positive\n")
+} else if (number == 0) {
+    print("Zero\n")
+} else {
+    print("Negative\n")
+}
+
+
+// Ternary operator
+var isRaining: Bool = true
+print(isRaining ? "Drive safely\n" : "It's not raining, enjoy your trip!\n")
+
+
+// Comparison and boolean operators
+print(1 <= 4)
+print(!isRaining)
+print((4+5 >= 9  && false) || true  ? "asdf" : "fdsa")
