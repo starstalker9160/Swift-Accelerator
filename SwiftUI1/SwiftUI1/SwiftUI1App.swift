501058b514857cf1548f1976fb78f00d1fd6b916
starstalker9160 <abyazjschool@gmail.com>
2025-07-10 18:28:28 +0900

    asdf

---
+++ SwiftUI1/SwiftUI1/SwiftUI1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI1App.swift
+//  SwiftUI1
+//
+//  Created by star.stalker9160 on 10/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI1/SwiftUI1UITests/SwiftUI1UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUI1UITestsLaunchTests.swift
+//  SwiftUI1UITests
+//
+//  Created by star.stalker9160 on 10/07/2025.
+//
+
+import XCTest
+
+final class SwiftUI1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
