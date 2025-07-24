7d235600073027ed08f0585469445d680eb614dc
starstalker9160 <abyazjschool@gmail.com>
2025-07-24 15:11:23 +0400

    10

---
+++ Speeding Calculator/Speeding Calculator/Speeding_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speeding_CalculatorApp.swift
+//  Speeding Calculator
+//
+//  Created by star.stalker9160 on 24/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Speeding_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Speeding Calculator/Speeding CalculatorTests/Speeding_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Speeding_CalculatorTests.swift
+//  Speeding CalculatorTests
+//
+//  Created by star.stalker9160 on 24/07/2025.
+//
+
+import Testing
+@testable import Speeding_Calculator
+
+struct Speeding_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
