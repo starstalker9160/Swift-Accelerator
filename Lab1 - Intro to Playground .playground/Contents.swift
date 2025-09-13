e1b3d0d964d7596576e7a6d7a3fcca7a1dcef8c2
starstalker9160 <abyazjschool@gmail.com>
2025-09-13 12:05:52 +0400

    finished 1

---
+++ Lab1 - Intro to Playground .playground/Contents.swift
@@ -0,0 +1,27 @@
+/*:
+ 
+ The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
+ */
+print("I have learned the following:")
+print("What features make Swift a modern and safe language")
+print("How to use the Swift REPL in Terminal")
+print("How to use playgrounds to make writing Swift fun and simple")
+print("\n\n")
+/*:
+ Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
+ */
+let songName = "'Where No One Goes'"
+var artist = "John Powell and Jónsi"
+
+print(songName + " by " + artist)
+print("\n\n")
+/*:
+ Use multiple `print` functions to write out some of the lyrics to the song.
+ */
+print("""
+Let the wind carry us
+To the clouds, hurry up, alright
+We can travel so far
+As our eyes can see
+""")
+print("\n\n")
--- Playground.playground/Contents.swift
+++
@@ -1,6 +0,0 @@
-import UIKit
-
-let songName = "'Where No One Goes'"
-var artist = "John Powell and Jónsi"
-
-print(songName + " by " + artist)
