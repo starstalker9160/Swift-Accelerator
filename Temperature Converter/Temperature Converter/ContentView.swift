b06e7f6cb0ef8287ca5149fd3e47da0aebbb2deb
starstalker9160 <abyazjschool@gmail.com>
2025-11-01 09:14:50 +0400

    1.7

--- Temperature Converter/Temperature Converter/ContentView.swift
+++ Temperature Converter/Temperature Converter/ContentView.swift
@@ -8,20 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
-    let c = 40.0
-    let f = 40.0
-    
-    var cToF: Double { c * 9/5 + 32 }
-    var fToC: Double { (f - 32) * 5/9 }
+    @State private var cToF_: Bool = true
+    @State private var input: String = ""
+    @State private var out: String = ""
     
     var body: some View {
         VStack {
             Text("Temperature")
                 .bold()
                 .font(.title)
-            Text(" ")
-            Text("\(String(format: "%.2f", c))℃ - \(String(format: "%.1f", cToF))℉")
-            Text("\(String(format: "%.2f", f))℉ - \(String(format: "%.1f", fToC))℃")
+
+            // Drop down menu
+            Menu {
+                Button("Celsius to Fahrenheit") {
+                    cToF_ = true
+                    out = ""
+                }
+                Button("Fahrenheit to Celsius") {
+                    cToF_ = false
+                    out = ""
+                }
+            } label: {
+                Label("Select an Option", systemImage: "chevron.down")
+                    .padding()
+                    .background(Color.blue.opacity(0.2))
+                    .cornerRadius(8)
+            }
+
+            TextField("Enter \(cToF_ ? "℃" : "℉")", text: $input)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+                .keyboardType(.decimalPad)
+
+            if !out.isEmpty {
+                Text(out)
+                    .font(.title2)
+                    .padding(.top)
+            }
+
+            Button("Convert") { convert() }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+        }
+        .padding()
+    }
+
+    // moved the button logic into a function because app would crash if i try to do this inline
+    func convert() {
+        // copied this from google, this ensures the data is a valid float
+        guard let value = Double(input) else {
+            out = "Invalid input"
+            return
+        }
+
+        if cToF_ {
+            out = "\(String(format: "%.2f", value * 9 / 5 + 32)) ℉"
+        } else {
+            out = "\(String(format: "%.2f", (value - 32) * 5 / 9)) ℃"
         }
     }
 }
