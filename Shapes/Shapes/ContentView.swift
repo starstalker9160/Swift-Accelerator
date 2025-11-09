87ee292bd0e707ae5f11cf14c30fd225ab266fef
starstalker9160 <abyazjschool@gmail.com>
2025-11-09 11:29:35 +0400

    6

---
+++ Shapes/Shapes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shapes
+//
+//  Created by star.stalker9160 on 09/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shapes/Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by star.stalker9160 on 09/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
