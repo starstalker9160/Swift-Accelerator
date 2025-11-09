231afd20d7f3f99260725caecdc26478892ac740
starstalker9160 <abyazjschool@gmail.com>
2025-11-09 11:29:45 +0400

    6 (again)

--- Shapes/Shapes/ContentView.swift
+++ Shapes/Shapes/ContentView.swift
@@ -8,12 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedShape = "Circle"
+    let shapes = ["Rectangle", "Circle", "Capsule", "Ellipse"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 20) {
+            Text("Shapes")
+                .font(.title)
+                .bold()
+            Picker("Choose a Shape", selection: $selectedShape) {
+                ForEach(shapes, id: \.self) { shape in
+                    Text(shape)
+                }
+            }
+            .pickerStyle(DefaultPickerStyle())
+            .padding()
+            
+            if selectedShape == "Rectangle" {
+                Rectangle()
+                    .fill(Color.blue)
+                    .frame(width: 200, height: 150)
+            } else if selectedShape == "Circle" {
+                Circle()
+                .fill(Color.pink)
+                .frame(width: 200, height: 200)
+            } else if selectedShape == "Ellipse" {
+                Ellipse()
+                .fill(Color.green)
+                .frame(width: 250, height: 120)
+            } else if selectedShape == "Capsule" {
+                Capsule()
+                .fill(Color.orange)
+                .frame(width: 250, height: 80)
+            }
         }
         .padding()
     }
